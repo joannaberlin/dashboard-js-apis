2228275f1cdf05fb796e43cdd6e7038f530d96c4
joannaberlin <joanna.kot.ok@gmail.com>
2024-04-13 16:30:03 +0200

    Add error catching, fetch crypto currencies data from another api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const url = process.env.URL;
+const defUrl = process.env.DEFURL;
 const authNameWrapper = document.getElementById('img-auth');
 
 fetch(url)
@@ -7,4 +8,14 @@ fetch(url)
 		console.log(data);
 		document.body.style.backgroundImage = `url(${data.urls.full})`;
 		authNameWrapper.innerHTML = `By: ${data.user.name}`;
+	})
+	.catch((err) => {
+		console.log(err);
+		console.log('something went wrong!');
+		document.body.style.backgroundImage = `url(${defUrl})`;
 	});
+
+fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+	.then((res) => res.json())
+	.then((data) => console.log(data))
+	.catch((err) => console.log(err));
