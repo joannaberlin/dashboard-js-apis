49245d4885b956bbcac033079474105eff582fc3
joannaberlin <joanna.kot.ok@gmail.com>
2024-04-13 13:08:06 +0200

    Render image author's name, add styles

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const url = process.env.URL;
+const authNameWrapper = document.getElementById('img-auth');
 
 fetch(url)
 	.then((res) => res.json())
 	.then((data) => {
-		console.log(data.urls.full);
+		console.log(data);
 		document.body.style.backgroundImage = `url(${data.urls.full})`;
+		authNameWrapper.innerHTML = `By: ${data.user.name}`;
 	});
