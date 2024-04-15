11e46b0813341e1d38bd075dfc582c0d7670b465
joannaberlin <joanna.kot.ok@gmail.com>
2024-04-15 18:03:34 +0200

    Render crypto values from api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const url = process.env.URL;
 const defUrl = process.env.DEFURL;
 const authNameWrapper = document.getElementById('img-auth');
 const cryptoWrapper = document.getElementById('crypto-top');
+const cryptoElement = document.getElementById('crypto');
 
 fetch(url)
 	.then((res) => res.json())
@@ -27,6 +28,11 @@ fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
 		cryptoWrapper.innerHTML = `
 			<img src=${data.image.small} alt="${data.name} icon" />
 			<span>${data.name}</span>
+		`;
+		cryptoElement.innerHTML += `
+		<p>🎯: $${data.market_data.current_price.usd}</p>
+		<p>👆: $${data.market_data.high_24h.usd}</p>
+		<p>👇: $${data.market_data.low_24h.usd}</p>
 		`;
 	})
 	.catch((err) => console.log(err));
