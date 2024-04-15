4e2bb4dfa63a4a054fffdfdbc235e16e53c46c56
joannaberlin <joanna.kot.ok@gmail.com>
2024-04-15 17:57:35 +0200

    Render crypto icon and name from api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const url = process.env.URL;
 const defUrl = process.env.DEFURL;
 const authNameWrapper = document.getElementById('img-auth');
+const cryptoWrapper = document.getElementById('crypto-top');
 
 fetch(url)
 	.then((res) => res.json())
@@ -16,6 +17,16 @@ fetch(url)
 	});
 
 fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
-	.then((res) => res.json())
-	.then((data) => console.log(data))
+	.then((res) => {
+		if (!res.ok) {
+			throw Error('something went wrong');
+		}
+		return res.json();
+	})
+	.then((data) => {
+		cryptoWrapper.innerHTML = `
+			<img src=${data.image.small} alt="${data.name} icon" />
+			<span>${data.name}</span>
+		`;
+	})
 	.catch((err) => console.log(err));
